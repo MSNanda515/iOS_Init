10bfecbd74bd9c50e2adf79fa0785414908f3216
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-08-08 17:32:46 -0600

    Made War CG UI

--- War_CardGame/War_CardGame/ContentView.swift
+++ War_CardGame/War_CardGame/ContentView.swift
@@ -9,13 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Spacer()
+            Image("logo")
+            Spacer()
+            HStack {
+                Spacer()
+                Image("card3")
+                Spacer()
+                Image("card4")
+                Spacer()
+            }
+            Spacer()
+            Image("dealbutton")
+            Spacer()
+            HStack {
+                Spacer()
+                VStack {
+                    Text("Player")
+                    Text("0")
+                        .padding(.top)
+                }
+                Spacer()
+                VStack {
+                    Text("CPU")
+                    Text("0")
+                        .padding(.top)
+                }
+                Spacer()
+            }
+            .font(.largeTitle)
+            .foregroundColor(Color.white)
+            Spacer()
+            
+        }
+        .background(
+            LinearGradient(gradient: Gradient(colors: [Color("bgColorTop"), Color("bgColorBot")]), startPoint: .top, endPoint: .bottom)
+        )
+        .ignoresSafeArea()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12")
     }
 }
