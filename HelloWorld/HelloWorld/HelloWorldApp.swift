e087d0df12aa7575a43303efc0ebec0562d0dfff
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-08-07 18:10:02 -0600

    Initial Commit

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Meharpreet Singh Nanda on 2021-08-07.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
