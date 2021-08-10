e4d9f7ef9cf0bd1e2e5ed1a80481dadd7d0d95b9
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-08-10 11:04:23 -0600

    Used variables, functions, structures and other related concepts in playground

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -18,3 +18,57 @@ let tax = 0.13
 var split:Double = 1.0*subtotal/people*(1+tax)
 
 print(split)
+
+// Functions
+func myFunc(a:Int) -> Int {
+    let b = 10
+    print(a+b)
+    return a+b
+}
+
+myFunc(a: 10)
+
+func goodMorning() {
+    print("Good Morning")
+}
+func printTotalWithTax(subtotal:Double){
+    print(1.13*subtotal)
+}
+func printTotalWithTax(subtotal:Double) -> Double {
+    return (1.13 * subtotal)
+}
+func calaculateTotalWithTax(subtotal:Double, tax:Double) -> Double {
+    return subtotal * tax
+}
+
+
+struct ChatView {
+//  Stored Properties
+    var message:String = ""
+//    Comuted property
+    var messageWithPrefix:String {
+        let prefix = "Chris Says: "
+        return prefix + message
+    }
+//    View Code for this screen
+//    Methods
+    func sendChat() {
+        print(message)
+    }
+    
+    func deleteChat() {
+        
+    }
+}
+
+// Sample model of a car
+struct Car {
+    private var make = "Honda", model = "Civic", year = "2010"
+    private var details:String {
+        return year + make + model
+    }
+    
+    public func getDetails() -> String {
+        return details
+    }
+}
