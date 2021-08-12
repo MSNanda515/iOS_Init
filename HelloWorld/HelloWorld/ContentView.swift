aa2513f86ed20d8d49f0263a902d303aa0a13cd4
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-08-12 15:08:53 -0600

    Learnt NavigationView, NavigationLink, and list

--- HelloWorld/HelloWorld/ContentView.swift
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -8,68 +8,87 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var items = ["Ele 1", "Ele 2", "Ele 3"]
+    
     var body: some View {
-       
-        VStack {
-            Spacer()
-            
-            ZStack {
-                Image("toronto")
-                    .cornerRadius(20)
-                    .blur(radius: 1.1)
-                VStack {
-                    Text("CN Tower")
-                        .font(.largeTitle)
-                        .padding([.top, .leading, .trailing])
-                    Text("Toronto")
-                        .font(.caption)
-                        .padding([.leading, .bottom, .trailing])
-                }
-                .background(Color.black.opacity(0.83))
-                .foregroundColor(.white)
-                .cornerRadius(20)
-                
+//        Learning Navigation
+        NavigationView {
+            List(items, id: \.self) { item in
+                NavigationLink(
+                    destination: Text("Destination"),
+                    label: {
+                        Text(item)
+                    })
             }
-            .padding()
-            Spacer()
-            ZStack {
-                Image("london")
-                    .cornerRadius(20)
-                    .blur(radius: 1.1)
-                VStack {
-                    Text("Big Ben")
-                        .font(.largeTitle)
-                        .padding([.top, .leading, .trailing])
-                    Text("London")
-                        .font(.caption)
-                        .padding([.leading, .bottom, .trailing])
-                }
-                .background(Color.black.opacity(0.83))
-                .foregroundColor(.white)
-                .cornerRadius(20)
-                
-            }
-            Spacer()
-            // The thing like the arrow function is called closure
-            Button("But1", action: {
-                print("Button 1")
-            })
-            // Button view with trailing closure
-            Button("But2") {
-                print("Button 2")
-            }
-            // Button instance with label image
-            Button(action: {
-                print("Button 3")
-            }, label: {
-                HStack {
-                    // Uses the SF images already available in Xcode
-                    Image(systemName: "pencil")
-                    Text("Button3")
-                }
-                
-            })
+            .navigationTitle(Text("MY LIST"))
         }
+                   
+        // Basic Concepts
+//        VStack {
+//            Spacer()
+//
+////            ZStack {
+////                Image("toronto")
+////                    .cornerRadius(20)
+////                    .blur(radius: 1.1)
+////                VStack {
+////                    Text("CN Tower")
+////                        .font(.largeTitle)
+////                        .padding([.top, .leading, .trailing])
+////                    Text("Toronto")
+////                        .font(.caption)
+////                        .padding([.leading, .bottom, .trailing])
+////                }
+////                .background(Color.black.opacity(0.83))
+////                .foregroundColor(.white)
+////                .cornerRadius(20)
+////
+////            }
+////            .padding()
+////            Spacer()
+//////            ZStack {
+//////                Image("london")
+//////                    .cornerRadius(20)
+//////                    .blur(radius: 1.1)
+//////                VStack {
+//////                    Text("Big Ben")
+//////                        .font(.largeTitle)
+//////                        .padding([.top, .leading, .trailing])
+//////                    Text("London")
+//////                        .font(.caption)
+//////                        .padding([.leading, .bottom, .trailing])
+//////                }
+//////                .background(Color.black.opacity(0.83))
+//////                .foregroundColor(.white)
+//////                .cornerRadius(20)
+//////
+//////            }
+////            Spacer()
+////            // The thing like the arrow function is called closure
+////            Button("But1", action: {
+////                print("Button 1")
+////            })
+////            // Button view with trailing closure
+////            Button("But2") {
+////                print("Button 2")
+////            }
+////            // Button instance with label image
+////            Button(action: {
+////                print("Button 3")
+////            }, label: {
+////                HStack {
+////                    // Uses the SF images already available in Xcode
+////                    Image(systemName: "pencil")
+////                    Text("Button3")
+////                }
+////
+////
+////            })
+//
+////
+//
+//        } // End VStack
         
     }
 }
