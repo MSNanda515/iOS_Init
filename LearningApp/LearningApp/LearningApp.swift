7a0fc8d0800bbe1166d5a1be21f4b861fb9de58d
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-08-25 14:42:00 -0600

    Set up the LearningApp project

---
+++ LearningApp/LearningApp/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Meharpreet Singh Nanda on 2021-08-25.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ LearningApp/LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by Meharpreet Singh Nanda on 2021-08-25.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+}
